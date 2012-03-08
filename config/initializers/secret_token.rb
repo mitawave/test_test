a83265cea51c4ec8bd62f742f727ca9661c997da
mitawave <mwave@ax.em-net.ne.jp>
2012-03-09 00:36:39 +0900

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestTest::Application.config.secret_token = '892b51556b6714a0b825280f215fc109d0a51f87676246b0b2903074d4a7ebd90a005b7f7e04feaf027ed6fc5523f9c4afef9af656c811373e28ec404b7d7711'
